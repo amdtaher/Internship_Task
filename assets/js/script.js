6b5e48548dc829843c19564e1544145ef127251f
Tanveer <101932506+amdtaher@users.noreply.github.com>
2023-11-10 01:35:04 +0600

    Task by Taher

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function myFunction(){
+    let x = document.querySelector("#navBar");
+    if(x.style.display === "block"){
+        x.style.display = "none";
+    }else{
+        x.style.display = "block";
+    }
+}
